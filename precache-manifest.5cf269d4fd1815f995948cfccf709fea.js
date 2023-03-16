f588ad140f9121750fe9760c73c9ee589aa10c61
vishalreddy75 <77541406+vishalreddy75@users.noreply.github.com>
2023-03-16 18:24:39 +0530

    Updates

---
+++ precache-manifest.5cf269d4fd1815f995948cfccf709fea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b576c81d381102a84a1",
+    "url": "/TextUtils-React/static/css/main.6a658268.chunk.css"
+  },
+  {
+    "revision": "6b576c81d381102a84a1",
+    "url": "/TextUtils-React/static/js/main.46c18795.chunk.js"
+  },
+  {
+    "revision": "a4f9459a308288b6214f",
+    "url": "/TextUtils-React/static/js/runtime~main.02905c84.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/TextUtils-React/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/TextUtils-React/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "6cf13d6485dca57e4c275a75ed43be5a",
+    "url": "/TextUtils-React/index.html"
+  }
+];
